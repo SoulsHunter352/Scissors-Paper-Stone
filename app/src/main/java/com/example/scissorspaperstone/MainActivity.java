42c3ac613339cceb5b8fbfaa7fbb6a4d281cf2f9
SoulsHunter352 <vovamaiorov11@gmail.com>
2025-09-11 22:42:32 +0300

    Добавлено сохранение настроек и переход между экранами

--- app/src/main/java/com/example/scissorspaperstone/MainActivity.java
+++ app/src/main/java/com/example/scissorspaperstone/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.scissorspaperstone;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ImageButton settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettings(v);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void openSettings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/scissorspaperstone/SettingsActivity.java
+++ app/src/main/java/com/example/scissorspaperstone/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.example.scissorspaperstone;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -11,17 +14,38 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private SharedPreferences mPrefs;
+    private int currentWinPoints;
+    private TextView winPointsText;  // Текст, отображающий кол-во очков
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
+
+        mPrefs = getSharedPreferences(getLocalClassName(), MODE_PRIVATE);
+        currentWinPoints = mPrefs.getInt("win_points", 1);
+
         SeekBar seekBar = findViewById(R.id.win_points_seekbar);
-        TextView winPoints = findViewById(R.id.win_points_value);
+        seekBar.setProgress(currentWinPoints - 1);
+
+        winPointsText = findViewById(R.id.win_points_value);
+        updateWinPointsText(currentWinPoints);
+
+        ImageButton back_button = findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickBackButton(v);
+            }
+        });
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                winPoints.setText(String.valueOf(progress + 1));
+                updateWinPointsText(progress + 1);
             }
 
             @Override
@@ -31,7 +55,7 @@ public class SettingsActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                updateWinPoints(seekBar.getProgress());
             }
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -39,5 +63,25 @@ public class SettingsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor ed = mPrefs.edit();
+        ed.putInt("win_points", currentWinPoints);
+        ed.apply();
+    }
+
+    public void updateWinPoints(int value){
+        currentWinPoints = value + 1;
+    }
+
+    public void updateWinPointsText(int value){
+        winPointsText.setText(String.valueOf(value));
+    }
+
+    public void clickBackButton(View view){
+        finish();
     }
 }
